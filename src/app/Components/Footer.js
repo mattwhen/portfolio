7cec8003a181f0c5c1933b1148809a6366286d18
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-07-25 06:42:53 -0500

    This commit adds transition styling to the anchor links at top of page, customizing footer section, and adding hamburger file that will be used for mobile layout

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -8,11 +8,9 @@ import {
 const Footer = () => {
 	return (
         <footer className='flex flex-col justify-center  bg-slate-600 py-10 font-bold'>
-			<div className=' text-center mb-5 text-sm'>
-				© 2023 Matthew A. Nguyen. All Rights Reserved.
-			</div>
 
-			<div className='flex justify-center gap-10 text-4xl'>
+                <p className='text-lg flex justify-center mb-5'>Follow me on Social Media:</p>
+			<div className='flex justify-center items-center gap-10 text-4xl'>
 				<a href='https://github.com/mattwhen/' target='_blank'>
 					<AiFillGithub />
 				</a>
@@ -25,6 +23,9 @@ const Footer = () => {
 				<a href='https://twitter.com/MatthewxNguyen' target='_blank'>
 					<AiFillTwitterCircle />
 				</a>
+			</div>
+			<div className=' text-center mt-8 text-sm'>
+				© 2023 Matthew A. Nguyen. All Rights Reserved.
 			</div>
         </footer>
 
