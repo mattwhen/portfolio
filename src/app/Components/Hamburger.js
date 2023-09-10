f570f7480bbeef7d527c84d8697204a81358f6b3
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2023-09-10 14:30:37 -0500

    This commit adds our mobile hamburger icon in mobile layout, re-arranging content, minor styling, and adding media queries in tailwind config file

--- src/app/Components/Hamburger.js
+++ src/app/Components/Hamburger.js
@@ -1,11 +1,65 @@
+"use client";
+
 import React from 'react';
-import RxHamburgerMenu from 'react-icons';
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { RxCross1 } from 'react-icons/rx';
+import '../globals.css';
 
 const Hamburger = () => {
+	// useState returns an array with exactly two values
+	// 1. The current state.
+	// 2. The set function that lets you update the state to a different value
+	const [navOpen, setNavOpen] = useState(false);
+
+	// // Prevent/hide scrolling when the Navigation menu is opened.
+	if (navOpen) {
+		document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+	} else {
+		document.getElementsByTagName('body')[0].style.overflow = 'visible';
+	}
+
 	return (
 		<>
-			<RxHamburgerMenu />
-
+					<div className='hamburger-icon'>
+						<a
+							href='#home'
+							className='text-4xl flex align-middle text-gray-500 md:hidden lg:hidden'
+							onClick={() => (navOpen ? setNavOpen(false) : setNavOpen(true))}
+						>
+							{navOpen ? (
+								<RxCross1 className='z-50 transition-all' />
+							) : (
+								<GiHamburgerMenu className='' />
+							)}
+						</a>
+					</div>
+				<section className='js-mobile-menu md:hidden lg:hidden'>
+					<ul
+						className={navOpen ? 'show-mobile-nav bg-white' : 'hide-mobile-nav'}
+					>
+						<li className=' border-gray-400'>
+							<a
+								href='#resume'
+								onClick={() => {
+									document.body.scrollTop = 0;
+									document.documentElement.scrollTop = 0;
+									return navOpen ? setNavOpen(false) : setNavOpen(true);
+								}}
+							>
+								RESUME
+							</a>
+						</li>
+						<li className=' border-gray-400 '>
+							<a
+								href='#projects'
+								onClick={() => (navOpen ? setNavOpen(false) : setNavOpen(true))}
+							>
+								PROJECTS
+							</a>
+						</li>
+					</ul>
+				</section>
 		</>
 	);
 };
