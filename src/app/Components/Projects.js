c47f48a76f06b947de2d9108cce993c36e35b80f
Matthew Nguyen <76631936+mattwhen@users.noreply.github.com>
2024-01-09 21:25:59 -0600

    Updated with Movie Catalog (RMC) project in the Project section along with including links to deployed app and Github repo

--- src/app/Components/Projects.js
+++ src/app/Components/Projects.js
@@ -2,33 +2,33 @@ import React from 'react';
 import { FiExternalLink } from 'react-icons/fi';
 import { AiFillGithub } from 'react-icons/ai';
 import Image from 'next/image';
-import cultureCruncher from '/public/images/cultureCruncher-live-app.png';
+import RealMovieCatalog from '/public/images/movie-catalog.png';
 import thingamabobs from '/public/images/treethingamabobs-live-screenshot.png';
 import fetchcoin from '/public/images/fetchcoin.png';
 
 const Projects = () => {
 	const cardData = [
 		{
-			name: ['Fetchcoin', 'Culture Cruncher', 'Thingamabobs'],
+			name: ['Fetchcoin', 'Reel Movie Catalog', 'Thingamabobs'],
 			description: [
 				'A web application that integrates the Postman API to display current prices of the top cryptocurrencies.',
-				'A web application that displays specific about a country such ascurrency and population. Renders a Youtube video that displays acommon cuisine in that region.',
+				'Reel Movie Catalog (RMC) is a forthcoming web application focused on providing users with a user-friendly platform to access and explore a diverse array of movies from a dynamic database. The application aims to eliminate the hassle of selecting the perfect movie by offering detailed information such as summaries, cast details, ratings, and reviews. Users can search for specific movie titles and tailor their results based on keywords, enhancing their ability to make informed decisions and stay updated on their favorite films..',
 				'A full stack e-commerce application that allows users to browse a catalog of items, and proceed to check out. Utilizes user authentcation, which would allow the user to view the entire catalog once a user is signed in.',
 			],
-			img: [cultureCruncher, thingamabobs, fetchcoin],
+			img: [fetchcoin, RealMovieCatalog, thingamabobs],
 			github: [
 				'https://github.com/mattwhen/fetchcoin',
-				'https://github.com/dbradley555/culture-cruncher',
+				'https://github.com/mattwhen/movie-catalog',
 				'https://github.com/treecao/thingamabobs',
 			],
 			deployment: [
 				'https://mattwhen.github.io/fetchcoin/',
-				'https://dbradley555.github.io/culture-cruncher/',
+				'movie-catalog-ecru.vercel.app',
 				'https://treethingamabobs.herokuapp.com/',
 			],
 			alt: [
 				'fetchcoin project screenshot',
-				'Culture Cruncher project screenshot',
+				'Movie Catalog project screenshot',
 				'Thingamabobs project screenshot',
 			],
 		},
@@ -47,7 +47,7 @@ const Projects = () => {
 											<a href={cardData[0].github[0]}>
 												<Image
 													className='rounded-lg'
-													src={cardData[0].img[2]}
+													src={cardData[0].img[0]}
 													alt='fetchcoin project screenshot'
 												/>
 											</a>
@@ -58,12 +58,20 @@ const Projects = () => {
 										<p className='px-5'>{cardData[0].description[0]}</p>
 										<div className='flex justify-around py-8'>
 											<div className='flex gap-10'>
-												<a href={cardData[0].github[0]} target='_blank' className='hover:text-sky-500'>
+												<a
+													href={cardData[0].github[0]}
+													target='_blank'
+													className='hover:text-sky-500'
+												>
 													<span className='px-2'>Github</span>
 													<AiFillGithub className='text-3xl display: inline-block' />
 												</a>
 
-												<a href={cardData[0].deployment[0]} target='_blank' className='hover:text-sky-500'>
+												<a
+													href={cardData[0].deployment[0]}
+													target='_blank'
+													className='hover:text-sky-500'
+												>
 													<span className='px-2'>Try it out!</span>
 													<FiExternalLink className='text-3xl display: inline-block' />
 												</a>
@@ -86,12 +94,20 @@ const Projects = () => {
 										<p className='px-5'>{cardData[0].description[1]}</p>
 										<div className='flex justify-around py-8'>
 											<div className='flex gap-10'>
-												<a href={cardData[0].github[1]} target='_blank' className='hover:text-sky-500'>
+												<a
+													href={cardData[0].github[1]}
+													target='_blank'
+													className='hover:text-sky-500'
+												>
 													<span className='px-2'>Github</span>
 													<AiFillGithub className='text-3xl display: inline-block' />
 												</a>
 
-												<a href={cardData[0].deployment[1]} target='_blank' className='hover:text-sky-500'>
+												<a
+													href={cardData[0].deployment[1]}
+													target='_blank'
+													className='hover:text-sky-500'
+												>
 													<span className='px-2'>Try it out!</span>
 													<FiExternalLink className='text-3xl display: inline-block' />
 												</a>
@@ -103,7 +119,7 @@ const Projects = () => {
 											<a href={cardData[0].github[2]}>
 												<Image
 													className='rounded-lg'
-													src={cardData[0].img[0]}
+													src={cardData[0].img[2]}
 													alt='fetchcoin project screenshot'
 												/>
 											</a>
@@ -114,12 +130,20 @@ const Projects = () => {
 										<p className='px-5'>{cardData[0].description[2]}</p>
 										<div className='flex justify-around py-8'>
 											<div className='flex gap-10'>
-												<a href={cardData[0].github[2]} target='_blank' className='hover:text-sky-500'>
+												<a
+													href={cardData[0].github[2]}
+													target='_blank'
+													className='hover:text-sky-500'
+												>
 													<span className='px-2'>Github</span>
 													<AiFillGithub className='text-3xl display: inline-block' />
 												</a>
 
-												<a href={cardData[0].deployment[2]} target='_blank' className='hover:text-sky-500'>
+												<a
+													href={cardData[0].deployment[2]}
+													target='_blank'
+													className='hover:text-sky-500'
+												>
 													<span className='px-2'>Try it out!</span>
 													<FiExternalLink className='text-3xl display: inline-block' />
 												</a>
@@ -130,120 +154,6 @@ const Projects = () => {
 							);
 						})}
 					</div>
-					{/* <div className='text-center max-w-sm py-10 md:max-w-3xl flex flex-col gap-y-20'>
-						<div id='project-1' className='p-5 shadow-md '>
-							<div className='flex justify-center'>
-								<a
-									href='https://mattwhen.github.io/fetchcoin/'
-									target='_blank'
-									className='md:w-3/4'
-								>
-									<Image
-										className='rounded-lg'
-										src={fetchcoin}
-										alt='fetchcoin project screenshot'
-									/>
-								</a>
-							</div>
-							<h2 className='text-gray-800 py-8 text-2xl'>Fetchcoin</h2>
-							<p className='text-md py-5'>
-								A web application that integrates the Postman API to display
-								current prices of the top cryptocurrencies.
-							</p>
-							<div className='flex justify-around py-8'>
-								<div className='flex gap-10'>
-									<a
-										href='https://github.com/mattwhen/fetchcoin'
-										target='_blank'
-									>
-										<span className='px-2'>Github</span>
-										<AiFillGithub className='text-3xl display: inline-block' />
-									</a>
-
-									<a
-										href='https://mattwhen.github.io/fetchcoin/'
-										target='_blank'
-									>
-										<span className='px-2'>Try it out!</span>
-										<FiExternalLink className='text-3xl display: inline-block' />
-									</a>
-								</div>
-							</div>
-						</div>
-						<div id='project-2' className=' p-5 shadow-md hover:bg-zinc-100'>
-							<a
-								href='https://dbradley555.github.io/culture-cruncher/'
-								target='_blank'
-							>
-								<Image
-									className='rounded-lg'
-									src={cultureCruncher}
-									alt='cultureCruncher project screenshot'
-								/>
-							</a>
-							<h2 className='text-gray-800 py-8 text-2xl'>Culture Cruncher</h2>
-							<p className='text-md py-5'>
-								A web application that displays specific about a country such as
-								currency and population. Renders a Youtube video that displays a
-								common cuisine in that region.
-							</p>
-							<div className='flex justify-around py-8'>
-								<div className='flex gap-10'>
-									<a
-										href='https://github.com/dbradley555/culture-cruncher'
-										target='_blank'
-									>
-										<span className='px-2'>Github</span>
-										<AiFillGithub className='text-3xl display: inline-block' />
-									</a>
-
-									<a
-										href='https://dbradley555.github.io/culture-cruncher/'
-										target='_blank'
-									>
-										<span className='px-2'>Try it out!</span>
-										<FiExternalLink className='text-3xl display: inline-block' />
-									</a>
-								</div>
-							</div>
-						</div>
-
-						<div id='project-3' className=' p-5 shadow-md hover:bg-zinc-100'>
-							<a href='https://github.com/treecao/thingamabobs' target='_blank'>
-								<Image
-									className='rounded-lg'
-									src={thingamabobs}
-									alt='e-commerce landing page screenshot'
-								/>
-							</a>
-							<h2 className='text-gray-800 py-8 text-2xl'>Thingamabobs</h2>
-							<p className='text-md py-5'>
-								A full stack e-commerce application that allows users to browse
-								a catalog of items, and proceed to check out. Utilizes user
-								authentication, which would allow the user to view the entire
-								catalog once a user is signed in.
-							</p>
-							<div className='flex justify-around py-8'>
-								<div className='flex gap-10'>
-									<a
-										href='https://github.com/treecao/thingamabobs'
-										target='_blank'
-									>
-										<span className='px-2'>Github</span>
-										<AiFillGithub className='text-3xl display: inline-block' />
-									</a>
-
-									<a
-										href='https://treethingamabobs.herokuapp.com/'
-										target='_blank'
-									>
-										<span className='px-2'>Try it out!</span>
-										<FiExternalLink className='text-3xl display: inline-block' />
-									</a>
-								</div>
-							</div>
-						</div>
-					</div> */}
 				</div>
 			</section>
 		</div>
